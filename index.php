b51fcbe1c3f1b24d89d4f131c33d25b46cea1e84
Sojamilch <hlrockwelljones@gmail.com>
2021-11-09 15:30:10 +0000

    Created artist input page   database query works

--- index.php
+++ index.php
@@ -7,16 +7,15 @@ include('assets/templates/header.php')
 <body>
 
     <img src="assets/images/froger.jpg"></img>
+
+    <div class="d-inline-flex p-2">
     <form action="scripts/databaseQuery.php" method="post">
         <div class="form-group">
-            <label for="exampleInputPassword1">name</label>
+            <label for="exampleInputPassword1">Name</label>
             <input type="password" class="form-control" id="exampleInputPassword1" name="name" placeholder="Password">
         </div>
-        <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
-
+    </div>
 </body>
--- scripts/databaseQuery.php
+++ scripts/databaseQuery.php
@@ -1,28 +1,22 @@
 <?php 
 
-$servername = "strode-digital.uk";
+$servername = "localhost";
 $username = "harrison1";
 $password = "0000";
 $dbname = "harrisonTest";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
 if($conn === false){
     die("ERROR: Could not connect. " 
         . mysqli_connect_error());
 }
 
+
 $name = $_REQUEST['name'];
 
-$sql = "INSERT INTO table1 (name) VALUES ('$name')";
+$sql = "INSERT INTO table1 (name) VALUES ('$name');";
 
-if (mysqli_query($conn, $sql)){
-
-    echo"<h3> Data Uploaded</h3>";
-
-} else{
-    echo "ERROR: oops $sql" . mysqli_error($conn);
-}
 
 mysqli_close($conn);
 
