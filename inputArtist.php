aa2575748792050e7b9a34b5bd4d4a6d88bf81b1
Sojamilch <hlrockwelljones@gmail.com>
2021-11-12 12:30:17 +0000

    Fixed input form appeareance

--- inputArtist.php
+++ inputArtist.php
@@ -1,38 +1,40 @@
 <?php
 
-include('assets/templates/header.php')
-
+include('assets/templates/header.php');
+session_start();
 ?>
 <body>
-    <div style="margin-left: 25%; margin-right: 25%; margin-top: 50px; " id="adminForm">
+    <div class="d-flex justify-content-center align-items-center container mt-5" id="adminForm">
+        <div class="row">
                 <h1> Add a Painting </h1>
                 <form method="post" action="/scripts/dataSubmitController.php" enctype="multipart/form-data">
-                    <div class="form-group">
-                        <label for="paintingName">Painting Name</label>
+                    <div class="mb-3">
+                        <label for="paintingName" class="form-label">Painting Name</label>
                         <input name="paintingName" type="text" class="form-control" id="paintingName" aria-describedby="paintingNameHelp" placeholder="Enter painting name">
                         <small id="paintingNameHelp" class="form-text text-muted">whats the paintings name</small>
                     </div>
-                    <div class="form-group">
-                        <label for="paintingDescription">Paiting Description</label>
+                    <div class="mb-3">
+                        <label for="paintingDescription" class="form-label" >Paiting Description</label>
                         <input name="paintingDescription" type="text" class="form-control" id="paintingDescription" aria-describedby="descriptionHelp" placeholder="Enter Painting Description">
                     </div>
-                    <div class="form-group">
-                        <label for="artistName">Artist Name</label>
+                    <div class="mb-3">
+                        <label for="artistName" class="form-label" >Artist Name</label>
                         <input name="artistName" type="text" class="form-control" id="artistName" aria-describedby="artistHelp" placeholder="Enter Artist Name">
                     </div>
-                    <div class="form-group">
-                        <label for="price">Paiting Price</label>
+                    <div class="mb-3">
+                        <label for="price" class="form-label" >Paiting Price</label>
                         <input name="paintingPrice" type="number" class="form-control" id="price" aria-describedby="pricehelp" placeholder="Enter Painting Price">
                     </div>
-                    <div class="form-group">
-                        <label for="paintingSize">Size</label>
+                    <div class="mb-3">
+                        <label for="paintingSize" class="form-label" >Size</label>
                         <input name="paintingSize" type="text" class="form-control" id="paintingSize" aria-describedby="siezHelp" placeholder="Enter Painting Size">
                     </div>
-                    <div class="form-group">
-                        <label for="paintingImage">Upload Image</label>
+                    <div class="mb-3">
+                        <label for="paintingImage" class="form-label" >Upload Image</label>
                         <input name="paintingImage" type="file" class="form-control" id="paintingImage" accept="image/png" >
                     </div>
-                    <input type="submit" class="btn btn-primary" value="inputData" name="inputData"></input>
+                    <input type="submit" class="btn btn-primary" value="Upload" name="inputData"></input>
                 </form>
-            </div>
+        </div>
+    </div>
 </body>
