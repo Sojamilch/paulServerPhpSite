2697e016ad4766aa4da3fad4a0164260fc73024e
Sojamilch <hlrockwelljones@gmail.com>
2021-11-18 13:20:15 +0000

    added profile page

---
+++ profile.php
@@ -0,0 +1,21 @@
+<?php
+
+include ('assets/templates/header.php');
+
+if ($_SESSION['loggedin'] != TRUE) 
+{
+    echo"<script> location.href = 'index.php'</script>";
+}
+
+?>
+
+<body>
+
+     
+    <div class="d-flex justify-content-center align-items-center container mt-5">
+
+    
+    </div>
+
+
+</body>
