053c7f4c0595f8a0ba1a6726bfd950a0e30d1888
Sojamilch <hlrockwelljones@gmail.com>
2021-11-18 08:54:04 -0800

    adde d p

--- profile.php
+++ profile.php
@@ -15,9 +15,9 @@ if ($_SESSION['loggedin'] != TRUE)
     <div class="d-flex justify-content-center align-items-center container mt-5">
 
 
-        <h5> Username: <?php echo $_SESSION['name'] ?> <br></br></h5> 
+        <p> Username: <?php echo $_SESSION['name'] ?></p> 
         
-        <h5> ID:  <?php echo $_SESSION['id'] ?> </h5>
+        <p> ID:  <?php echo $_SESSION['id'] ?> </p>
 
     
     </div>
